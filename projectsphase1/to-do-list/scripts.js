94e649d048833a1b02da5f1008b78936935ce67c
code_hopper <ameykhondekar01@gmail.com>
2024-03-21 19:44:47 +0530

    added task

--- projectsphase1/to-do-list/scripts.js
+++ projectsphase1/to-do-list/scripts.js
@@ -1,18 +1,66 @@
-// collect form data
+$(document).ready(() => {
 
-let form = $("#task")
+    // craeting variables to extract data from a form using JQ
 
-// array to store all the task
-let tasks = []
+    let taskName, taskTime, taskDisc = ""
 
-// each task will be a object 
-// {
-//     TaskName: "some name",
-//     TaskTime: "some time",
-//     TaskDetails: "some details"
-// }
+    // we will create a object of this data to add it in a array
 
-form.submit((event)=>{
-    event.preventDefault()
-    console.log(event)
+    let taskObject = {}
+
+    // a array containing all task objects
+
+    let tasks = []
+
+    $("#taskForm").submit((event) => {
+        event.preventDefault()
+
+        // console.log(event)
+
+        taskName = $('[name=taskName]').val()
+        taskTime = $('[name=taskTime]').val()
+        taskDisc = $('[name=taskDetails]').val()
+
+        taskObject = {
+            name: taskName,
+            time: taskTime,
+            details: taskDisc
+        }
+
+        console.log(taskObject)
+
+        tasks.push(taskObject)
+
+        console.log(tasks)
+
+        // now append each child into html page of array
+
+        $('#display-all-task').append(() => {
+            return (
+
+                tasks.map((task) => {
+
+                    return (
+                        `
+                        <div class="card my-3">
+                            <div class="card-header">
+                                <h1 class="card-title">${task.name}</h1>
+                                <button class="btn btn-success">Done <i class="fa-solid fa-check"></i> </button>
+                                <button class="btn btn-danger">Delete <i class="fa-solid fa-trash"></i></button>
+                            </div>
+                            <div class="card-body">
+                                <p class="card-text">${task.details}</p>
+                                <h4>${task.time}</h3>
+                            </div>
+                        </div>
+                        `
+                    )
+
+                })
+
+            )
+        })
+
+    })
+
 })
